d8e0c8aa5da6ddc9afbd4ede72f7a6ee89fc4a5b
Ivan Huerta <ihuerta6125@yahoo.com>
2022-07-06 14:33:00 -0500

    end of class wednesday

--- lectures/week01/3-Wednesday/MY-LectureNotes/formatting.py
+++ lectures/week01/3-Wednesday/MY-LectureNotes/formatting.py
@@ -1,23 +1,23 @@
 
 # Create a variable called first_name and set the value to your first name
-
+first_name = "Ivan"
 # create a variable called last_name and set the value to your last name
-
+last_name = "Huerta"
 # print your first name console
-
+print (first_name)
 # print your last name to the console
-
+print (last_name)
 # Create a new varaiable called my_name and set the value to the concatenation of your first name and your last name 
-
+my_name = (first_name + "." + last_name)
 # print "My name is ???" out to the console.  Substitue the ??? with the my_name variable.
-
+print (My name is my_name)
 # create a variable called company_name and set the value to "DigitalCrafts"
-
+company_name = DigitalCrafts
 
 # Using the two variables you set earlier, generate a company email address that follows the pattern: "first name, period, last name @ company domain". Assign the result to a new variable called email and print the email to the console.
 # harry.potter@DigitalCrafts.com
-
-
+email = "(first_name,.,last_name@DigitalCrafts)"
+print = email
 
 # Using the firstname and lastname variables, generate a username to a new variable and print it to the console. The username should follow this format: first name, underscore, lastname.
 # harry_potter
